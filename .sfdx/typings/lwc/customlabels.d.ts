1356002faed0290f830dc2aeffc6cc1ef39ff2c1
Jana <soujana@deloitte.com>
2019-04-11 12:53:30 +0530

    Demo message

--- .sfdx/typings/lwc/customlabels.d.ts
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -1,4 +1,12 @@
-declare module "@salesforce/label/c.TestLabel" {
-    var TestLabel: string;
-    export default TestLabel;
+declare module "@salesforce/label/c.SFDXDEMOLABEL" {
+    var SFDXDEMOLABEL: string;
+    export default SFDXDEMOLABEL;
+}
+declare module "@salesforce/label/c.SFDXDEMOLABEL11April" {
+    var SFDXDEMOLABEL11April: string;
+    export default SFDXDEMOLABEL11April;
+}
+declare module "@salesforce/label/c.SFDXDEMOLABEL2" {
+    var SFDXDEMOLABEL2: string;
+    export default SFDXDEMOLABEL2;
 }
